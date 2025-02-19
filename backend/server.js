4fcc0dae504b5feb1352dade3acecdfa4becfc13
riboldicamila <cami.riboldi@gmail.com>
2025-02-19 10:42:17 +0100

    mern project setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,42 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.models.js";
 
-const app= express();
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); //middleware function to allow json
 
 app.get("/", (req, res) => {
-    res.send("Server ready.")
-})
+  res.send("Server ready.");
+});
 
-app.listen(5000, () => {
-    console.log("Server started at... port 5000 ")
-})
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ sucess: false, message: "Provide all fields." });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ sucess: true, data: newProduct });
+  } catch (error) {
+    console.log("Error in creation of product.", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+
+console.log(process.env.MONGO_URL);
+
+app.listen(5001, () => {
+  connectDB();
+  console.log("Server started at... port 5000 ");
+});
