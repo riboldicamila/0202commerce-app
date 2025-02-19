f9db4781b8e9b49056b209b543c45fc691565515
riboldicamila <cami.riboldi@gmail.com>
2025-02-19 11:23:58 +0100

    mern project setup

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,17 @@ app.get("/", (req, res) => {
   res.send("Server ready.");
 });
 
+app.get("/api/products", async (req, res) => {
+    try {
+        const products= await Product.find({});
+        res.status(200).json({ sucess: true, data: products });
+      } catch (error) {
+        console.log("Error in fetch of product.", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+      }
+  });
+  
+
 app.post("/api/products", async (req, res) => {
   const product = req.body;
 
@@ -32,6 +43,20 @@ app.post("/api/products", async (req, res) => {
     res.status(500).json({ success: false, message: "Server Error" });
   }
 });
+
+app.delete("/api/products/:id", async (req, res) => {
+    
+    const {id} = req.params;
+    console.log("id:", id);
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted"})
+    }
+    catch (error){
+        res.status(400).json({ success: false, message: "Error deleting. Product not found."})
+    }
+})
 
 
 console.log(process.env.MONGO_URL);
